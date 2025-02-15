041973d562c586daa8ed9d93fc7b238a183d4d6e
radubogdan6466 <radubogdan6466@gmail.com>
2025-02-15 16:20:30 +0200

    Update website

--- src/pages/exam/pdfs/PdfFiles.js
+++ src/pages/exam/pdfs/PdfFiles.js
@@ -1,22 +1,22 @@
 import React from "react";
+import "./PdfFiles.css";
 
 const pdfFiles = [
-  { name: "intrebari", path: "/pdfs/intrebari.pdf" },
-{name:"m_curs1", path: "/pdfs/m_curs1.pdf"},
-{name:"m_curs2", path: "/pdfs/m_curs2.pdf"},
-{name:"m_curs3", path: "/pdfs/m_curs3.pdf"},
-{name:"m_curs4", path: "/pdfs/m_curs4.pdf"},
-{name:"m_curs5", path: "/pdfs/m_curs5.pdf"},
-{name:"m_curs6", path: "/pdfs/m_curs6.pdf"},
-{name:"m_curs7", path: "/pdfs/m_curs7.pdf"},
-{name:"m_curs8", path: "/pdfs/m_curs8.pdf"},
-{name:"m_curs9", path: "/pdfs/m_curs9.pdf"},
-{name:"m_curs10", path: "/pdfs/m_curs10.pdf"},
-{name:"m_curs11_UC_2_0", path: "/pdfs/m_curs11_UC_2_0.pdf"},
-{name:"m_curs12", path: "/pdfs/m_curs12.pdf"},
-{name:"m_curs13", path: "/pdfs/m_curs13.pdf"},
-{name:"m_curs14", path: "/pdfs/m_curs14.pdf"},
-
+  { name: "Curs 1", path: "/pdfs/m_curs1.pdf" }, // Am schimbat "m_curs1" cu "Curs 1"
+  { name: "Curs 2", path: "/pdfs/m_curs2.pdf" }, // Am schimbat "m_curs2" cu "Curs 2"
+  { name: "Curs 3", path: "/pdfs/m_curs3.pdf" },
+  { name: "Curs 4", path: "/pdfs/m_curs4.pdf" },
+  { name: "Curs 5", path: "/pdfs/m_curs5.pdf" },
+  { name: "Curs 6", path: "/pdfs/m_curs6.pdf" },
+  { name: "Curs 7", path: "/pdfs/m_curs7.pdf" },
+  { name: "Curs 8", path: "/pdfs/m_curs8.pdf" },
+  { name: "Curs 9", path: "/pdfs/m_curs9.pdf" },
+  { name: "Curs 10", path: "/pdfs/m_curs10.pdf" },
+  { name: "Curs 11", path: "/pdfs/m_curs11_UC_2_0.pdf" }, // Aici poți decide cum vrei să afișezi
+  { name: "Curs 12", path: "/pdfs/m_curs12.pdf" },
+  { name: "Curs 13", path: "/pdfs/m_curs13.pdf" },
+  { name: "Curs 14", path: "/pdfs/m_curs14.pdf" },
+  { name: "Întrebări", path: "/pdfs/intrebari.pdf" }, // Am schimbat "intrebari" cu "Întrebări"
 ];
 
 const PdfPage = () => {
@@ -27,7 +27,7 @@ const PdfPage = () => {
         {pdfFiles.map((file, index) => (
           <li key={index}>
             <a href={file.path} target="_blank" rel="noopener noreferrer">
-              {file.name}
+              {file.name} {/* Acum se va afișa "Curs 1", "Curs 2" etc. */}
             </a>
           </li>
         ))}
